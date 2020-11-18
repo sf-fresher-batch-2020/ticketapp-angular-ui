b7e126f05b2c6d5fbc7a0e1e1a1ca27d63b42b77
tushantkumar <tushantgarg75@gmail.com>
2020-11-18 17:18:06 +0530

    added editticketdetail feature

---
+++ projects/frontend/src/app/components/editticketdetail/editticketdetail.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { TicketService } from '../ticket.service';
+
+@Component({
+  selector: 'app-editticketdetail',
+  templateUrl: './editticketdetail.component.html',
+  styleUrls: ['./editticketdetail.component.css']
+})
+export class EditticketdetailComponent implements OnInit {
+
+  id: number;
+  ticket: any;
+  constructor(private route: ActivatedRoute, private ticketService: TicketService, private authService: AuthService) {
+    this.route.params.subscribe(params => {
+      this.id = +params["id"];
+    })
+  }
+  selectedTicket;
+  ngOnInit(): void {
+    this.loadTicket();
+  }
+
+
+  loadTicket() {
+    this.ticketService.getTicket(this.id).subscribe(res => {
+      this.ticket = res;
+    })
+  }
+
+  
+  updateTicket(ticket){
+    this.ticketService.update(ticket).subscribe(res => {
+       this.loadTicket();
+       window.location.href="myticket";
+    })
+  }
+
+  deleteTicket(id) {
+    console.log("Deleting user :" + id);
+    this.ticketService.deleteTicket(id).subscribe(res => {
+      this.selectedTicket= null; // reset the data;
+      window.location.href="myticket";
+    // refresh the data
+    });
+  }
+  
+ /* closeticket(ticket){
+    ticket["ticketstatus"] = "Close";
+    this.ticketService.updateStatus(ticket).subscribe(res => {
+       this.loadTicket();
+    })
+  }*/
+
+}
