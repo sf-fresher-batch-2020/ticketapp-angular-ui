594f1f552397f69d57183346f58e2351c49cae4c
tushantkumar <tushantgarg75@gmail.com>
2020-11-18 17:00:36 +0530

    add userticket feature

---
+++ projects/frontend/src/app/components/userticket/userticket.component.ts
@@ -0,0 +1,48 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../auth.service';
+import { TicketService } from '../ticket.service';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-userticket',
+  templateUrl: './userticket.component.html',
+  styleUrls: ['./userticket.component.css']
+})
+export class UserticketComponent implements OnInit {
+
+  constructor(private ticketService: TicketService, private router: Router,
+    private authService: AuthService, private userService: UserService, private toastr: ToastrService) { }
+
+  loggedInUser;
+
+  ngOnInit(): void {
+    this.loggedInUser = this.authService.getLoggedInUser();
+  }
+  title: string;
+  mobile: string;
+  department: string;
+  priority: string;
+  description: string;
+  ticketstatus: string;
+  team: string;
+
+  ticket() {
+    let ticketData = {
+      title: this.title, mobilenumber: this.mobile,
+      department: this.department, priority: this.priority, description: this.description,
+      ticketstatus: 'Open', team: '', createdBy: this.loggedInUser.id, createdDate : new Date().toJSON()
+    };
+    console.log(JSON.stringify(ticketData));
+    this.ticketService.ticket(ticketData).subscribe(res => {
+      console.log(res);
+      this.toastr.success("Successfully Generated Ticket");
+      this.router.navigate(['myticket']);
+    }, err => {
+      console.error(err);
+      this.toastr.error("Failed To Generate Ticket");
+    });
+  }
+}
