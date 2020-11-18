308c1d174d271a8426a7b432a6247a90bf632ff2
tushantkumar <72964792+tushantkumar@users.noreply.github.com>
2020-11-18 17:24:35 +0530

    update service

---
+++ projects/frontend/src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.API_URL;
+  }
+
+  getUsers() {
+    let url = this.apiUrl + "/users";
+    return this.http.get(url);
+  }
+
+
+  getUser(id) {
+    let url = this.apiUrl + "/users/" + id;
+    return this.http.get(url);
+  }
+
+  deleteUser(id) {
+    let url = this.apiUrl + "/users/" + id;
+    return this.http.delete(url);
+  }
+  register(user) {
+    let url = this.apiUrl + "/users";
+    return this.http.post(url, user);
+  }
+}
