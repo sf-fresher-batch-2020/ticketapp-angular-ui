65a359644cf27d88f49435919579ff6818121c7b
tushantkumar <72964792+tushantkumar@users.noreply.github.com>
2020-11-18 17:29:46 +0530

    updated modules

--- projects/frontend/src/app/app-routing.module.ts
+++ projects/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent }  from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ListuserComponent } from './listuser/listuser.component';
+import { UserticketComponent } from './userticket/userticket.component';
+import { MyticketComponent } from './myticket/myticket.component';
+import { ViewticketdetailComponent } from './viewticketdetail/viewticketdetail.component';
+import { EditticketdetailComponent } from './editticketdetail/editticketdetail.component';
+import { TechdashboardComponent } from './techdashboard/techdashboard.component';
+import { UserticketdetailComponent } from './userticketdetail/userticketdetail.component';
+import { EdituserticketdetailComponent } from './edituserticketdetail/edituserticketdetail.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
-  {path:'', redirectTo:'home', pathMatch:'full' }
+  {path:'register', component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'listuser',component:ListuserComponent},
+  {path:'userticket',component:UserticketComponent},
+  {path:'myticket',component:MyticketComponent},
+  {path:'viewticketdetail/:id',component:ViewticketdetailComponent},
+  {path:'editticketdetail/:id',component:EditticketdetailComponent},
+  {path:'techdashboard',component:TechdashboardComponent},
+  {path:'userticketdetail/:id',component:UserticketdetailComponent},
+  {path:'edituserticketdetail/:id',component:EdituserticketdetailComponent},
+  {path:'', redirectTo:'login', pathMatch:'full'}
 ];
 
 @NgModule({
