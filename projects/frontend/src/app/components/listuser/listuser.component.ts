faf0dd8e407debfb846562d8717bf386dfdce1e7
tushantkumar <tushantgarg75@gmail.com>
2020-11-20 10:22:42 +0530

    added listuser feature

---
+++ projects/frontend/src/app/components/listuser/listuser.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+
+@Component({
+  selector: 'app-listuser',
+  templateUrl: './listuser.component.html',
+  styleUrls: ['./listuser.component.css']
+})
+export class ListuserComponent implements OnInit {
+
+  constructor(private userService: UserService) { }
+
+  user: any;
+  selectedUser;
+
+  ngOnInit(): void {
+    this.listUsers();
+  }
+  listUsers() {
+    console.log("Fetch All users from mock backend api");
+    this.userService.getUsers().subscribe(res => {
+      let users:any = res;
+      this.user = users.filter(obj => obj.role == "USER");
+    });
+  }
+ /* view(id) {
+    this.selectedUser = null; //reset
+    console.log("Fetch  users from mock backend api for the given userId ", id);
+    this.userService.getUser(id).subscribe(res => {
+      this.selectedUser = res;
+    });
+  }
+  deleteUser(id) {
+    console.log("Deleting user :" + id);
+    this.userService.deleteUser(id).subscribe(res => {
+      this.selectedUser = null; // reset the data;
+      this.listUsers(); // refresh the data
+    });
+  }*/
+}
